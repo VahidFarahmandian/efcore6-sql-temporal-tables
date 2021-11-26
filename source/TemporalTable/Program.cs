a92bbe4ff33cb234c88bdeaca9a5ef99042bb480
Vahid Farahmandian <farahmandian2011@gmail.com>
2021-11-26 14:53:16 +0330

    documents added

--- source/TemporalTable/Program.cs
+++ source/TemporalTable/Program.cs
@@ -7,19 +7,38 @@ foreach (var item in ctx.Products.ToList())
 }
 
 Console.WriteLine("*********TemporalAll************");
-foreach (var item in ctx.Products.TemporalAll().Select(x => new { x.Name, x.Price, ValidFrom = EF.Property<DateTime>(x, "PeriodStart"), ValidTo = EF.Property<DateTime>(x, "PeriodEnd") }))
+foreach (var item in
+    ctx.Products
+    .TemporalAll()
+    .Select(x => new
+    {
+        x.Name,
+        x.Price,
+        ValidFrom = EF.Property<DateTime>(x, "PeriodStart"),
+        ValidTo = EF.Property<DateTime>(x, "PeriodEnd")
+    })
+    )
 {
     Console.WriteLine($"{item},From:{item.ValidFrom}, To:{item.ValidTo} ");
 }
 
 Console.WriteLine("********TemporalBetween************");
-foreach (var item in ctx.Products.TemporalBetween(DateTime.MinValue, new DateTime(2021, 11, 22, 16, 00, 00)).Select(x => new { x.Name, x.Price, ValidFrom = EF.Property<DateTime>(x, "PeriodStart"), ValidTo = EF.Property<DateTime>(x, "PeriodEnd") }))
+foreach (var item in ctx.Products
+    .TemporalBetween(DateTime.MinValue, new DateTime(2021, 11, 22, 16, 00, 00))
+    .Select(x => new
+    {
+        x.Name,
+        x.Price,
+        ValidFrom = EF.Property<DateTime>(x, "PeriodStart"),
+        ValidTo = EF.Property<DateTime>(x, "PeriodEnd")
+    }))
 {
     Console.WriteLine($"{item},From:{item.ValidFrom}, To:{item.ValidTo} ");
 }
 
 Console.WriteLine("********TemporalAsOf*************");
-foreach (var item in ctx.Products.TemporalAsOf(new DateTime(2021, 11, 22, 16, 00, 00)).Select(x => new { x.Name, x.Price, ValidFrom = EF.Property<DateTime>(x, "PeriodStart"), ValidTo = EF.Property<DateTime>(x, "PeriodEnd") }))
+foreach (var item in ctx.Products
+    .TemporalAsOf(new DateTime(2021, 11, 22, 16, 00, 00)).Select(x => new { x.Name, x.Price, ValidFrom = EF.Property<DateTime>(x, "PeriodStart"), ValidTo = EF.Property<DateTime>(x, "PeriodEnd") }))
 {
     Console.WriteLine($"{item},From:{item.ValidFrom}, To:{item.ValidTo} ");
 }
